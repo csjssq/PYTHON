8dc889d934909849e797c0940ed71099a6e04d37
csjssq <67738390+csjssq@users.noreply.github.com>
2020-07-13 23:57:31 +0800

    Add files via upload
    error

---
+++ hw4.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib
+from http import cookiejar
+from bs4 import BeautifulSoup
+
+cj = http.cookiejar.CookieJar()
+opener = urllib.build_opener(urllib.HTTPCookieProcessor(cj))
+urllib.install_opener(opener)
+
+postdata_login = urllib.urlencode({
+    'id': 'davidstark',
+    'pw': 'davidstark',
+    'submit': 'login'
+})
+req = urllib.request.Request(url='https://bbs.sjtu.edu.cn/bbslogin', data=postdata_login)
+response = urllib.request.urlopen(req)
+
+text = 'hellobbs12345'
+postdata = urllib.urlencode({
+    'text': text,
+    'type': 'update'
+})
+
+req = urllib.request.Request(url='https://bbs.sjtu.edu.cn/bbsplan', data=postdata)
+response = urllib.request.urlopen(req)
+
+content = urllib.request.urlopen('https://bbs.sjtu.edu.cn/bbsplan').read()
+soup = BeautifulSoup(content, features="html.parser")
+print (str(soup.find('textarea').string).strip().decode('utf8'))
